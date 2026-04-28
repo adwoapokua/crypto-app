cdca8a0c60dd790a64f12c429083e7912574d5be
adwoapokua <adwoapaboagye@gmail.com>
2026-04-28 11:11:13 +0000

    created auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
-import { login, register } from "../controllers/authController";
-import protect from "../middleware/authMiddleware"
+import { login, register } from "../controllers/authController.js";
+import protect from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
